eb5faa7f9bffd6996980997674c6abb5e82b7e75
fuanjia <cfuanjia@gmail.com>
2021-08-31 04:23:11 -0700

    holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,10 +1,19 @@
-#include<stdio.h>
+#include <unistd.h>
+#include "holberton.h"
 
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
 int main(void)
 {
-	/* simple 
-	* code
-	*/
-	printf("Holberton\n");
-	return 0;
+	char word[9] = "Holberton";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
 }
